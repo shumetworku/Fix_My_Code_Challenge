1802db7875656e58e9e74af9afc36d76294362fe
shumetworku <shumwor@gmail.com>
2023-05-22 04:58:48 -0700

    fix my code

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -20,9 +20,9 @@ def fizzbuzz(n):
         if (i % 3) == 0:
             tmp_result.append("Fizz")
         elif (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+            tmp_result.append("FizzBuzz")
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
